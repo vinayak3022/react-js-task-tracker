cf50641ee06f17bcf24d83efb972abec7c9d9b54
vinayak.singh <vinayak.singh@flipkart.com>
2021-04-29 14:56:38 +0530

    connected to backend and added router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import Button from "./Button";
+import {useLocation} from "react-router-dom";
+
 const Header = ({onAdd, showAdd}) => {
+    const location = useLocation()
+
     return(
         <header className='header'>
             <h1>Task Tracker</h1>
-            <Button
+            {location.pathname === '/' && <Button
                 color={showAdd ? 'red' : 'green'}
                 text={showAdd ? 'Close' : 'Add'}
                 onclick={onAdd}>
-            </Button>
+            </Button>}
         </header>
     )
 }
